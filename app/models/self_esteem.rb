f38e695b7220c0337c0c038b8aa5d1cdc593bd04
Necromos <masternecromos@gmail.com>
2015-03-28 16:17:56 +0100

    updated self esteem validation model

--- app/models/self_esteem.rb
+++ app/models/self_esteem.rb
@@ -1,8 +1,8 @@
 class SelfEsteem < ActiveRecord::Base
   belongs_to :personal_datum
 
-  validates :alcohol, :tabacco, :drugs, :walking_time_per_day, :jogging_time_per_day, :gym_workout_time_per_day, :wholesome_food_per_day, :junk_food_per_day, :weather, :season, :self_esteem, presence: true
+  validates :alcohol, :tabacco, :drugs, :walking_time_per_day, :jogging_time_per_day, :gym_workout_time_per_day, :swimming_time_per_day, :wholesome_food_per_day, :junk_food_per_day, :weather, :season, :self_esteem, presence: true
   validates :tabacco, :drugs, :self_esteem, numericality: { only_integer: true }
-  validates :alcohol, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-  validates :walking_time_per_day, :jogging_time_per_day, :gym_workout_time_per_day, :wholesome_food_per_day, :junk_food_per_day, numericality: { only_integer: true }
+  validates :alcohol, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
+  validates :swimming_time_per_day, :walking_time_per_day, :jogging_time_per_day, :gym_workout_time_per_day, :wholesome_food_per_day, :junk_food_per_day, numericality: { only_integer: true }
 end
