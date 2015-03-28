206c0118a442f867c7e791ddd451708a59fd46fa
Necromos <masternecromos@gmail.com>
2015-03-28 20:39:04 +0100

    controller bugfix

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -121,6 +121,6 @@ class SurveyController < ApplicationController
     end
 
     def self_esteem_params
-      params.require(:self_esteem).permit(:personal_datum_id, :alcohol, :tabacco, :drugs, :walking_time_per_day, :jogging_time_per_day, :gym_workout_time_per_day, :wholesome_food_per_day, :junk_food_per_day, :weather, :season, :self_esteem)
+      params.require(:self_esteem).permit(:personal_datum_id, :alcohol, :tabacco, :drugs, :walking_time_per_day, :jogging_time_per_day, :gym_workout_time_per_day, :swimming_time_per_day, :wholesome_food_per_day, :junk_food_per_day, :weather, :season, :self_esteem)
     end
 end
