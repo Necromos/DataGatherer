8ab8b61bb49860e60c33b185827cd222803f839f
Necromos <masternecromos@gmail.com>
2015-05-31 16:14:53 +0200

    test.sh repair

--- tests.sh
+++ tests.sh
@@ -2,10 +2,10 @@
 echo "Disc on"
 rake classify:first_test[true]
 rake classify:second_test[10,100,true]
-rake classifu:third_test[true]
+rake classify:third_test[true]
 rake classify:fourth_test[true]
 echo "Disc off"
 rake classify:first_test[false]
 rake classify:second_test[10,100,false]
-rake classifu:third_test[false]
+rake classify:third_test[false]
 rake classify:fourth_test[false]
