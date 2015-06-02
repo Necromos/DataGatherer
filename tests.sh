bbc63ed2c35aaede6610c9cecc9529e461e145cb
Necromos <masternecromos@gmail.com>
2015-06-02 18:02:36 +0200

    update of tests script

--- tests.sh
+++ tests.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 echo "Disc on"
 rake classify:first_test[true]
-rake classify:second_test[10,100,true]
 rake classify:third_test[true]
 rake classify:fourth_test[true]
+echo "10000 runs 2-5 folds"
+for i in 2 3 4 5
+do
+  rake classify:second_test[$i,10000,true]
+  rake classify:fifth_test[$i,10000,true]
+  rake classify:sixth_test[$i,10000,true]
+done
 echo "Disc off"
 rake classify:first_test[false]
-rake classify:second_test[10,100,false]
 rake classify:third_test[false]
 rake classify:fourth_test[false]
+for i in 2 3 4 5
+do
+  rake classify:second_test[$i,10000,false]
+  rake classify:fifth_test[$i,10000,false]
+  rake classify:sixth_test[$i,10000,false]
+done
